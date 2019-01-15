dbb77b9e0386d4d8f26c4d70912a0ad9cd861ee2
ihuanglele <huanglele@yousuowei.cn>
2019-01-15 20:45:08 +0800

    添加 muse-ui 框架

---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>Search for You</p>
+    <mu-form>
+      <mu-form-item prop="input" label="Input">
+        <mu-text-field v-model="key"></mu-text-field>
+      </mu-form-item>
+    </mu-form>
+  </div>
+</template>
+
+<script>
+import MuForm from 'muse-ui/es5/Form/Form'
+import MuFormItem from 'muse-ui/es5/Form/FormItem'
+
+export default {
+  components: {
+    MuFormItem,
+    MuForm},
+  data () {
+    return {
+      key: ''
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
